741459f25bf990e9b9eb44b9bb6289aef6a170f4
SebiSomu <sebigsomu@gmail.com>
2025-11-01 18:00:01 +0200

    Added footer

--- Shop_Functionalities.js
+++ Shop_Functionalities.js
@@ -5,3 +5,17 @@ document.getElementById("menu")?.addEventListener("click", function() {
 document.getElementById("back")?.addEventListener("click", function() {
     window.location.href = "Shop_Structure.html";
 });
+
+document.addEventListener("DOMContentLoaded", async function() {
+    const footerPlaceholder = document.getElementById("footer-placeholder");
+
+    if (footerPlaceholder) {
+        try {
+            const response = await fetch("footer.html");
+            const data = await response.text();
+            footerPlaceholder.innerHTML = data;
+        } catch (err) {
+            console.error("Error during footer loading:", err);
+        }
+    }
+});
