ffc452a43c9bb44af9458e5c2328894e508329df
SebiSomu <sebigsomu@gmail.com>
2025-11-01 13:36:16 +0200

    Added Menu.html file and Back button in it

--- Shop_Functionalities.js
+++ Shop_Functionalities.js
@@ -1,4 +1,9 @@
-let menu_button = document.getElementById("menu");
-menu_button.addEventListener("click", function() {
+console.log("JS merge");
+
+document.getElementById("menu")?.addEventListener("click", function() {
     window.location.href = "Menu.html";
-})
+});
+
+document.getElementById("back")?.addEventListener("click", function() {
+    window.location.href = "Shop_Structure.html";
+});
